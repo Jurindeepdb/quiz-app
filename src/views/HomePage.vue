972b4f3bac879a9ed66dcb96eeb4338c1e05689a
Jurin Deep Das <jurindeepdas@gmail.com>
2025-02-02 18:41:44 +0530

    Create HomePage.vue

---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <button @click="$router.push('/quiz')">Start Quiz</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'  // Changed from 'Home' to 'HomePage'
+}
+</script>
+
+<style scoped>
+/* Add styles for Home page here */
+</style>
